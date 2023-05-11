629b627b6347f21937fe6ca77705114d6009ec93
Hasan Malik <hmalik0412@gmail.com>
2023-05-11 16:00:53 +0100

    Added questions to JS

--- Assets/JS/questions.js
+++ Assets/JS/questions.js
@@ -13,5 +13,41 @@ const quiz = [
       question: "How many Harry Potter books are there?",
       choices: ["4", "5", "6", "7"],
       answer: "7"
-    }
+    },
+    {
+      question: "What is the capital of Slovakia?",
+      choices: ["Bratislava", "Kiev", "Kosice", "Bucharest"],
+      answer: "Bratislava"  
+    },
+    {
+      question: "What year did Lewis Hamilton win his first Formula One World Championship?",
+      choices: ["2006", "2007", "2008", "2009"],
+      answer: "2008"  
+    },
+    {
+      question: "Who is the Prime Minister of the UK?",
+      choices: ["Rishi Sunak", "Boris Johnson", "David Cameron", "Tony Blair"],
+      answer: "Rishi Sunak"  
+    },
+    {
+      question: "How many NBA Chamionships have the Boston Celtics won?",
+      choices: ["7", "8", "17", "18"],
+      answer: "17"  
+    },
+    {
+      question: "Where did the 1996 Summer Olympics take place?",
+      choices: ["Los Angeles", "Atlanta", "Miami", "Seattle"],
+      answer: "Atlanta"  
+    },
+    {
+      question: "In which athletic competition does Wayde van Niekerk hold the World Record?",
+      choices: ["200m race", "400m race", "800m race", "1500m race"],
+      answer: "400m race"  
+    },
+    {
+      question: "Which political party is currently in government in the UK?",
+      choices: ["The Labour Party", "The Democrats", "The Republicans", "The Conservative Party"],
+      answer: "The Conservative Party"
+    },
+
   ]
