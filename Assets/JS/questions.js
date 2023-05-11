fd441b1afdb8edf6837b7fc857d4c202ea022b54
Hasan Malik <hmalik0412@gmail.com>
2023-05-11 15:36:26 +0100

    Added questions

---
+++ Assets/JS/questions.js
@@ -0,0 +1,17 @@
+const quiz = [
+    {
+      question: "Who is the fastest man ever?",
+      choices: ["Asafa Powell", "Usain Bolt", "Justin Gatlin", "Tyson Gay"],
+      answer: "Usain Bolt"
+    },
+    {
+      question: "Who is the current UFC Middleweight Champion?",
+      choices: ["Jon Jones", "Alex Pereira", "Leon Edwards", "Israel Adesanya"],
+      answer: "Israel Adesanya"
+    },
+    {
+      question: "How many Harry Potter books are there?",
+      choices: ["4", "5", "6", "7"],
+      answer: "7"
+    }
+  ]
